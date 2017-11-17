7aebb51dfadb1c9c18becfce78cd2daa090bdc46
Jociel <jcielsouza@gmail.com>
2017-11-17 01:13:46 -0200

    Estudo

--- exemplos/introduction.clj
+++ exemplos/introduction.clj
@@ -5,3 +5,58 @@
 	[username]
 	(swap! visitors conj username)
 	(str "Hello, " username)) (require 'clojure.java.io)
+
+
+(defn greeting
+	"Returns a greeting of the form 'Helo, username.'"
+	[username]
+	(str "Hello, " username))
+
+
+(defn greeting
+	"Returns a greeting of the form 'Helo, username.'
+	Default username is 'world'."
+	([] (greeting "world"))
+	([username] (str "Hello, " username)))
+
+
+(defn date [person-1 person-2 & chaperones]
+	(println person-1 "and" person-2 "went out with" (count chaperones) "chaperones"))
+
+
+(defn date2 [person-1 person-2 & chaperones]
+	(println chaperones))
+
+(defn date3 [person-1 person-2 & chaperones]
+	(println person-1 "and" person-2 "went out with" (nth chaperones 0) "chaperones"))
+
+
+
+
+(defn indexable-word?
+	[word]
+	(> (count word) 2))
+
+(require '[clojure.string :as str])
+(filter indexable-word? (str/split "A fine day it is" #"\W+"))
+
+(filter (fn [w] (> (count w) 2)) (str/split "A fine day" #"\W+"))
+
+(filter #(> (count %) 2) (str/split "A fine day it is" #"\W+"))
+
+
+(defn indexable-words
+	[text]
+	(let [indexable-word? (fn [w] (> (count w) 2))]
+		(filter indexable-word? (str/split text #"\W+"))))
+
+
+
+
+(defn make-greeter
+	[greeting-prefix]
+	(fn [username] (str greeting-prefix ", " username)))
+
+(def hello-greeting (make-greeter "Hello"))
+
+(def aloha-greeting (make-greeter "Aloha"))
